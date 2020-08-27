610bc22c6a332c6f8d35b1f7a33e5d5587979593
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-08-27 10:36:17 -0400

    Rewrite and update test

--- packages/worker/src/handler.ts
+++ packages/worker/src/handler.ts
@@ -7,56 +7,60 @@ import {
 } from '@benzene/core';
 import { HandlerConfig } from './types';
 
-export async function handleRequest(
-  gql: GraphQL,
-  request: Request,
-  options: HandlerConfig = {}
-): Promise<Response> {
-  let requestBody: Record<string, any> | null = null;
+export function createHandler(gql: GraphQL, options: HandlerConfig = {}) {
+  async function handleRequest(request: Request) {
+    let requestBody: Record<string, any> | null = null;
 
-  if (request.method === 'POST') {
-    const oCtype = request.headers.get('content-type');
-    if (oCtype) {
-      try {
-        requestBody = parseBodyByContentType(await request.text(), oCtype);
-      } catch (err) {
-        return new Response(err.message, {
-          status: 400,
-          headers: { 'content-type': 'text/plain' },
-        });
+    if (request.method === 'POST') {
+      const oCtype = request.headers.get('content-type');
+      if (oCtype) {
+        try {
+          requestBody = parseBodyByContentType(await request.text(), oCtype);
+        } catch (err) {
+          return new Response(err.message, {
+            status: 400,
+            headers: { 'content-type': 'text/plain' },
+          });
+        }
       }
     }
-  }
 
-  const queryParams: { [key: string]: string } = {};
+    const queryParams: { [key: string]: string } = {};
 
-  new URLSearchParams(request.url.slice(request.url.indexOf('?'))).forEach(
-    (value, key) => (queryParams[key] = value)
-  );
-
-  const params = getGraphQLParams({
-    queryParams,
-    body: requestBody,
-  }) as HttpQueryRequest;
-  params.httpMethod = request.method;
-
-  try {
-    params.context =
-      typeof options.context === 'function'
-        ? await options.context(request)
-        : options.context || {};
-  } catch (err) {
-    err.message = `Context creation failed: ${err.message}`;
-    return new Response(
-      JSON.stringify(gql.formatExecutionResult({ errors: [err] })),
-      {
-        status: err.status || 500,
-        headers: { 'content-type': 'application/json' },
-      }
+    new URLSearchParams(request.url.slice(request.url.indexOf('?'))).forEach(
+      (value, key) => (queryParams[key] = value)
     );
+
+    const params = getGraphQLParams({
+      queryParams,
+      body: requestBody,
+    }) as HttpQueryRequest;
+    params.httpMethod = request.method;
+
+    try {
+      params.context =
+        typeof options.context === 'function'
+          ? await options.context(request)
+          : options.context || {};
+    } catch (err) {
+      err.message = `Context creation failed: ${err.message}`;
+      return new Response(
+        JSON.stringify(gql.formatExecutionResult({ errors: [err] })),
+        {
+          status: err.status || 500,
+          headers: { 'content-type': 'application/json' },
+        }
+      );
+    }
+
+    const { status, body, headers } = await runHttpQuery(gql, params);
+
+    return new Response(body, { status, headers });
   }
 
-  const { status, body, headers } = await runHttpQuery(gql, params);
-
-  return new Response(body, { status, headers });
+  return function handler(event: FetchEvent) {
+    if (options.path && options.path !== new URL(event.request.url).pathname)
+      return;
+    event.respondWith(handleRequest(event.request));
+  };
 }
--- packages/worker/src/types.ts
+++ packages/worker/src/types.ts
@@ -1,5 +1,6 @@
 import { ValueOrPromise, TContext } from '@benzene/core';
 
 export interface HandlerConfig {
+  path?: string;
   context?: TContext | ((request: Request) => ValueOrPromise<TContext>);
 }
