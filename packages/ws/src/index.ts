a892e7290dc22378ebd33c991158dd8082d74c57
Hoang Vo <g@hvvo.dev>
2020-10-25 16:17:04 -0400

    export SubscriptionConnection in ws

--- packages/ws/src/index.ts
+++ packages/ws/src/index.ts
@@ -1,3 +1,4 @@
 export * from '@benzene/core';
 export { createHandler as wsHandler } from './handler';
+export { SubscriptionConnection } from './connection';
 export { default as MessageTypes, GRAPHQL_WS } from './messageTypes';
