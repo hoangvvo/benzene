b190a089f03ab7a10ad90450c08ea970facd395f
Hoang Vo <dev+g@hvvo.dev>
2022-08-13 07:40:36 +0700

    Bump all deps

--- jest.config.ts
+++ jest.config.ts
@@ -6,4 +6,5 @@ export default {
   modulePathIgnorePatterns: ["/node_modules/"],
   testMatch: ["**/__tests__/**/*.spec.ts", "**/__tests__/**/*.test.ts"],
   coveragePathIgnorePatterns: ["/node_modules/", "/__tests__/", "/dist/"],
+  reporters: ["default", "github-actions"],
 };
